ee00d6d3aa1f9965fa4afa41fd924351713f0ab6
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-06-17 19:56:21 +0300

    rfactoring auth-reducers

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -12,30 +12,41 @@ const initialState = {
   isLoggedIn: false,
 };
 
+const registerFulfilledReducer = (state, action) => {
+  state.user = action.payload.user;
+  state.token = action.payload.token;
+  state.isLoggedIn = true;
+};
+
+const loginFulfilledReducer = (state, action) => {
+  state.user = action.payload.user;
+  state.token = action.payload.token;
+  state.isLoggedIn = true;
+};
+
+const logoutFulfieldeReducer = (state, action) => {
+  state.user = { name: null, email: null };
+  state.token = null;
+  state.isLoggedIn = false;
+};
+
+const fetchCurrentUserFulfilledReducer = (state, action) => {
+  state.user = action.payload;
+  state.isLoggedIn = true;
+};
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
-  extraReducers: {
-    [register.fulfilled](state, action) {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-      state.isLoggedIn = true;
-    },
-    [login.fulfilled](state, action) {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-      state.isLoggedIn = true;
-    },
-    [logout.fulfilled](state, action) {
-      state.user = { name: null, email: null };
-      state.token = null;
-      state.isLoggedIn = false;
-    },
-    [fetchCurrentUser.fulfilled](state, action) {
-      state.user = action.payload;
-      state.isLoggedIn = true;
-    },
-  },
+  extraReducers: builder =>
+    builder
+      .addCase(register.fulfilled, registerFulfilledReducer)
+      .addCase(login.fulfilled, loginFulfilledReducer)
+      .addCase(logout.fulfilled, logoutFulfieldeReducer)
+      .addCase(
+        fetchCurrentUser.fulfilled,
+        fetchCurrentUserFulfilledReducer
+      ),
 });
 
 export const authReducer = authSlice.reducer;
